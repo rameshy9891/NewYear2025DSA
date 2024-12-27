f39142aaeff1b6fc462992740929b4e4321d93ec
Ramesh Yadav <rameshy9891@gmail.com>
2024-12-27 13:44:39 +0530

    happy new print

---
+++ SimpleDSAproblem/src/com/ramesh/Happy.java
@@ -0,0 +1,11 @@
+package com.ramesh;
+import java.util.*;
+
+public class Happy {
+	
+	public static void main(String[] args) {
+		System.out.println("happy new year");
+	}
+	
+
+}
---
+++ SimpleDSAproblem/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rames
+ *
+ */
+module SimpleDSAproblem {
+}
