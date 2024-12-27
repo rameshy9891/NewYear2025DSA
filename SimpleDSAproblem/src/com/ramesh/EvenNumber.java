34011a148a8e9f726cb4193f012f4c5c0a68e740
Ramesh Yadav <rameshy9891@gmail.com>
2024-12-27 15:01:29 +0530

    Even number

---
+++ SimpleDSAproblem/src/com/ramesh/EvenNumber.java
@@ -0,0 +1,20 @@
+package com.ramesh;
+
+import java.util.Scanner;
+
+public class EvenNumber {
+	
+	public static void main(String[] args) {
+		System.out.println("write a number it will print greaten then 0 and below then that number");
+		Scanner sc = new Scanner(System.in);
+		int number= sc.nextInt();
+		
+		for(int i=1; i<=number ; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		sc.close();
+	}
+
+}
