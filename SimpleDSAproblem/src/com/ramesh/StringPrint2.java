695ce830a73a474e1450856e8d3abf04dce215a0
Ramesh Yadav <rameshy9891@gmail.com>
2025-01-15 20:13:50 +0530

    reverse the string

--- SimpleDSAproblem/src/com/ramesh/StringPrint2.java
+++ SimpleDSAproblem/src/com/ramesh/StringPrint2.java
@@ -15,6 +15,14 @@ public class StringPrint2 {
 		for(int i=0; i<=str.length()-1; i++) {
 			System.out.print(str.charAt(i));
 		}
+		
+		
+		System.out.println();
+		//reverse the string 
+		
+		for(int i=str.length()-1; i>=0; i--) {
+			System.out.print(str.charAt(i));
+		}
 	}
 
 }
